2d1e0175146a41aaf172543744890da902b595a0
dev-guy <devguy@devguy.com>
2016-07-21 09:16:52 -0700

    Static site

--- tests.bundle.js
+++
@@ -1,6 +0,0 @@
-//var context = require.context('.', true, /.+\.spec\.jsx?$/);
-var context = require.context('./test', true, /.+\.spec\.js?$/);
-
-console.log(context.keys());
-context.keys().forEach(context);
-module.exports = context;
