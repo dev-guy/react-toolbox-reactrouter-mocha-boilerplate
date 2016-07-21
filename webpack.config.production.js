4e9185a3359821df4a9fbe4461431e612e305046
Dev-Guy <devguy@devguy.com>
2016-07-21 10:30:22 -0700

    Add webpack.optimize.OccurenceOrderPlugin()

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -55,8 +55,8 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     // keeps hashes consistent between compilations
-    // new webpack.optimize.OccurenceOrderPlugin(),
-    // // minifies your code
+    new webpack.optimize.OccurenceOrderPlugin(),
+    // minifies your code
     new webpack.optimize.UglifyJsPlugin({
         compressor: {
         warnings: false
